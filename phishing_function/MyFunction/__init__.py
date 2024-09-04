1a671b46974c49b3b7f7593140d9a1f956ff4f7e
Mirandaet <miranda.thulstrup@gmail.com>
2024-09-04 15:06:12 +0200

    added specific cors origin

--- phishing_function/MyFunction/__init__.py
+++ phishing_function/MyFunction/__init__.py
@@ -10,10 +10,12 @@ import os
 # Set up Azure Cognitive Services client
 endpoint = "https://homaphising.cognitiveservices.azure.com/"
 key = os.environ['KEY']
-text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+text_analytics_client = TextAnalyticsClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key))
 
 # Fraud detection model endpoint
 model_endpoint = "http://d3251e64-1a14-46c8-b197-5541ab06a38e.swedencentral.azurecontainer.io/score"
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Processing upload...')
@@ -28,22 +30,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         sentiments = [doc.sentiment for doc in response]
 
         # Call the fraud detection model with processed features
-        model_response = requests.post(model_endpoint, json={"text": documents[0], "sentiments": sentiments})
+        model_response = requests.post(
+            model_endpoint, json={"text": documents[0], "sentiments": sentiments})
         result = model_response.json()
 
         headers = {
-                "Access-Control-Allow-Origin": "*",  # Allow requests from all domains (use a specific domain in production)
-                "Access-Control-Allow-Methods": "POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type"
-            }
+            # Replace with your frontend domain
+            "Access-Control-Allow-Origin": "https://jolly-bay-02c912b03.5.azurestaticapps.net",
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
+        }
 
         return func.HttpResponse(
-            json.dumps(result),
+            json.dumps({"message": "Request processed successfully"}),
+            status_code=200,
             mimetype="application/json",
             headers=headers
         )
     except Exception as e:
         logging.error(f"Error processing request: {e}")
         return func.HttpResponse(f"Error processing request: {e}", status_code=500)
-
-    
